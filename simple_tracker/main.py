0ad76169a96f42573d611758b8f357953be4b9c1
Your Name <you@example.com>
2025-11-06 14:12:31 +0400

    a

--- simple_tracker/main.py
+++ simple_tracker/main.py
@@ -1,6 +1,7 @@
 from tracker import Tracker
 import time
 
+print('a')
 with open('C:\\Users\\Student\\Desktop\\test_github\\config\\config.txt', 'r') as file:
     lines = file.readlines()
     interval_value = int(lines[0].split('=')[-1])
