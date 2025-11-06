a503d85e7274b749839610df6f4fed3727a4f582
Your Name <you@example.com>
2025-11-06 13:53:38 +0400

    feat: add infinite loop that tracks counts

--- simple_tracker/main.py
+++ simple_tracker/main.py
@@ -1,4 +1,16 @@
+from tracker import Tracker
+import time
+
 with open('C:\\Users\\Student\\Desktop\\test_github\\config\\config.txt', 'r') as file:
     lines = file.readlines()
     interval_value = int(lines[0].split('=')[-1])
+
+tracker = Tracker()
+
+while True:
+   tracker.increment() 
+   print(tracker.count)
+   tracker.save_to_file()
+   time.sleep(interval_value)
+
     
