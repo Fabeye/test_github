773b7ef8b82bd0efe91be01b5d126efe5d4f530a
Your Name <you@example.com>
2025-11-06 13:47:18 +0400

    feat: add Tracker class

--- simple_tracker/main.py
+++ simple_tracker/main.py
@@ -1,4 +1,4 @@
 with open('C:\\Users\\Student\\Desktop\\test_github\\config\\config.txt', 'r') as file:
     lines = file.readlines()
     interval_value = int(lines[0].split('=')[-1])
-    print(interval_value)
+    
